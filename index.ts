09afb1463c3869d9cabfb2d9fc9c4f22ca1e6a00
yurilabatsevich <yuralabatsevich@gmail.com>
2023-07-05 12:07:59 +0300

    init: start task

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import { httpServer } from "./src/http_server/index.js";
-
-const HTTP_PORT = 8181;
+import { httpServer } from "./src/http_server";
+export const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
